141fbd831946a3c948bf1e5427d76177ed563661
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-06-13 08:24:07 +0100

    conf: set up my db

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/devphaseX/hotel-reservation-api/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	uri         = "mongodb://localhost:27017"
+	dbName      = "hotel-reservation"
+	userColName = "users"
 )
 
 func main() {
 	listenAddress := flag.String("listenAddress", ":5000", "The listen address of the api server")
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userCol := client.Database(dbName).Collection(userColName)
+
+	user := types.User{
+		FirstName: "Ayomide",
+		LastName:  "Lawal",
+	}
+
+	res, err := userCol.InsertOne(context.Background(), user)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
 
 	flag.Parse()
 	app := fiber.New()
